a5bad316657761042527dee92e957c304ab048cc
leefordjudes <leefordjudes@gmail.com>
2026-09-07 16:35:37 +0530

    ghwf7

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -30,11 +30,11 @@ const config: Config = {
 
   // Set the production url of your site here
   // url: 'https://www.auditplus.io',
-  url: 'https://auditplus.github.io/aerp-docs',
+  url: 'https://auditplus.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   // baseUrl: '/docs/',
-  baseUrl: '/',
+  baseUrl: '/aerp-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
