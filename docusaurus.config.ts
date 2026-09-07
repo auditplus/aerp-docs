72c73fe38fa390a067d37df3d50f39f34130237a
Antony Judes <leefordjudes@gmail.com>
2026-09-07 17:33:19 +0530

    D1 (#21)

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -42,7 +42,7 @@ const config: Config = {
   projectName: 'aerp-docs', // Usually your repo name.
   deploymentBranch: 'main',
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',  // throw
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
@@ -58,7 +58,7 @@ const config: Config = {
       {
         docs: {
           sidebarPath: './sidebars.ts',
-          routeBasePath: '/',
+          routeBasePath: '/docs',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           // editUrl:
