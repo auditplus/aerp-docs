43adcadbd8c22b049340240b2ef85e842c6ab54e
Antony Judes <leefordjudes@gmail.com>
2026-09-07 16:30:59 +0530

    Gh5 (#17)

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -34,7 +34,7 @@ const config: Config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   // baseUrl: '/docs/',
-  baseUrl: '/aerp-docs/',
+  baseUrl: '/aerp-docs/docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
